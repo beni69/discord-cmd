6214694f3594e5e5ba694d787a127fd3e29427bd
beni69 <73666440+beni69@users.noreply.github.com>
2021-03-09 09:42:13 +0100

    :heavy_plus_sign: users can now create their own loggers

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import Command from "./modules/Command";
 import Handler from "./modules/Handler";
+import { Logger } from "./modules/Logging";
 
-export { Command, Handler };
+export { Command, Handler, Logger };
