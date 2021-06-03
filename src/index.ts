b415c6df5c60a353dd671e0c2d8a82bc0e86bc35
beni69 <73666440+beni69@users.noreply.github.com>
2021-06-03 13:42:56 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,4 @@
-import Command from "./modules/Command";
-import Handler from "./modules/Handler";
-import Logger from "./modules/Logging";
-import * as Utils from "./modules/Utils";
-
-export { Command, Handler, Logger, Utils };
+export { Command } from "./modules/Command";
+export { Handler } from "./modules/Handler";
+export * as Logger from "./modules/Logging";
+export * as Utils from "./modules/Utils";
